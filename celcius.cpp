997cdee3bc7c49ad2106fb81a9840b46997ac3eb
hackerbryan <bryan.ferrari@iis-silva-ricci.edu.it>
2025-12-18 13:24:48 +0100

    modifica 3

--- celcius.cpp
+++ celcius.cpp
@@ -7,12 +7,12 @@ using namespace std;
 float conversione (int gradi_c);
 int main () 
 {
-for (int i=0; i<=20; i++)
+for (int i=0; i<=100; i++)
 	{
 		cout <<i<<" gradi celsius sono equivalenti a : "<< conversione (i) <<" gradi fareneith"<< endl;
 	}
 }
 float conversione (int gradi_c)
 {
-	return 32+(9/5)*gradi_c;
+	return 50+(9/5)*gradi_c;
 }
